53f37a3a0e39462fb10ccc25e9816d671e679acd
GayaneNik238 <gnikoghosyan98@gmail.com>
2021-06-07 15:20:14 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {useState} from "react";
+import './App.css';
+import LessText from "./lessText"
+
+function App() {
+  return (
+    <div className="App">
+
+    <LessText text="lorem ndjcdncndjcdncjdncjdncjdncjdncdjncdndjcdncjdncjdncjdncjdncdjncdndjcdncjdncjdncjdncjdncdjncdndjcdncjdncjdncjdncjdncdjncdndjcdncjdncjdncjdncjdncdjncdndjcdncjdncjdncjdncjdncdjncdndjcdncjdncjdncjdncjdncdjncdndjcdncjdncjdncjdncjdncdjncdndjcdncjdncjdncjdncjdncdjncdndjcdncjdncjdncjdncjdncdjncdndjcdncjdncjdncjdncjdncdjncdndjcdncjdncjdncjdncjdncdjncdjdncjdncjdncjdncdjncd"
+    max={40}
+    />
+
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/lessText.js
@@ -0,0 +1,23 @@
+import {useState} from "react";
+
+function LessText({text, max = 55}) {
+
+    const [showLess, setShowLess] = useState(true);
+
+    if(text.length <= max) {
+        return <span>{text}</span>
+    } 
+
+return (
+    <span>
+        {showLess ? `${text.substring(0, max)} ...` : text}
+        <a href="#" onClick={(evt) => {
+            evt.preventDefault();
+            setShowLess(!showLess)
+        }} >{showLess ? "more" : "less"}</a>
+    </span>
+
+)
+}
+
+export default LessText;
